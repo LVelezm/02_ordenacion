2b357d3b1d253d733eff19eb7a713626bccca832
lvelezm <lvelezm@unjbg.edu.pe>
2024-09-13 22:07:51 -0500

    Cambio de las dos primeras funciones por error de entendimiento del algoritmo a realizar y nueva funcion que ordena con señal

--- interDirecto.cpp
+++ interDirecto.cpp
@@ -6,7 +6,7 @@ void interDirDer(int A[], int n){
 	int aux;
 	for(i=0; i<n-1; i++){
 		for(j=0; j<n-i-1; j++){
-			if (A[j]<A[j+1]){
+			if (A[j]>A[j+1]){
 				aux = A[j];
 				A[j] = A[j+1];
 				A[j+1] = aux;
@@ -19,19 +19,35 @@ void interDirDer(int A[], int n){
 void interDirIzq(int A[], int n){
 	int i, j;
 	int aux;
-	for(i=0; i<n-1; i++){
-		for(j=0; j<n-i-1; j++){
-			if (A[j]>A[j+1]){
-				aux = A[j];
-				A[j] = A[j+1];
-				A[j+1] = aux;
+	for(i=1; i<n; i++){
+		for(j=n; j<i; j--){
+			if (A[j]<A[j-1]){
+				aux = A[j-1];
+				A[j] = A[j];
+				A[j] = aux;
 			}
 			
 		}
 		
 	}
 }
-
+void interDirCen(int A[], int n){
+	int i = 0, j;
+    int cen = 1;
+    int aux;
+    do{
+        cen = 0;
+        for(j = 0; j < n - i - 1; j++) {
+            if(A[j] > A[j + 1]) {
+                aux = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = aux;
+                cen = 1;
+            }
+        }
+        i++;
+    } while(i < n - 1 && cen == 1);
+}
 int main(){
 	int i, numero, opcion;
 	bool menu=true;
@@ -54,21 +70,27 @@ int main(){
 		switch(opcion){
 			case 1:
 				interDirDer(Arreglo, numero);
-				cout << "Arreglo ordenado: " << endl;
-				for(i = 0; i < numero; i++){
+					cout << "Arreglo ordenado: " << endl;
+					for(i = 0; i < numero; i++){
 					cout << Arreglo[i] << " ";
-				}
-				cout<<endl;
+					}
+					cout<<endl;
 				break;
 			case 2:
 				interDirIzq(Arreglo, numero);
-				cout << "Arreglo ordenado: " << endl;
-				for(i = 0; i < numero; i++){
-					cout << Arreglo[i] << " ";
-				}
-				cout<<endl;
+					cout << "Arreglo ordenado: " << endl;
+					for(i = 0; i < numero; i++){
+						cout << Arreglo[i] << " ";
+					}
+					cout<<endl;
 				break;
 			case 3:
+				interDirCen(Arreglo, numero);
+					cout << "Arreglo ordenado: " << endl;
+					for(i = 0; i < numero; i++){
+						cout << Arreglo[i] << " ";
+					}
+					cout<<endl;
 				break;
 			case 4:
 				break;
